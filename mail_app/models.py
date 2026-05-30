579b038a4f94228539f1dada41bdb1a469f4e341
TalycNauur <sherstyukov.petya@gmail.com>
2026-05-30 12:26:21 +0300

    Add files via upload
    написал классификатор писем и закидывание письма в объект

---
+++ mail_app/models.py
@@ -0,0 +1,9 @@
+class MailMessage:
+
+    def __init__(self, path, text):
+        self.path = path
+        self.text = text
+
+    @property
+    def filename(self):
+        return self.path.name
